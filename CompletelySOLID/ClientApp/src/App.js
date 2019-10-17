10e59ac27c691e792bcf83c64ffe97049d0a1827
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-10-17 11:33:47 -0500

    added routing

--- CompletelySOLID/ClientApp/src/App.js
+++ CompletelySOLID/ClientApp/src/App.js
@@ -4,11 +4,15 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
+import MedicineContainer from './containers/Medicine';
+import PharmacyContainer from './containers/Pharmacy';
 
 export default () => (
   <Layout>
     <Route exact path='/home' component={Home} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    <Route path='/medicinelist' component={MedicineContainer}/>
+    <Route path='/pharmacylist' component={PharmacyContainer}/>
   </Layout>
 );
